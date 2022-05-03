775104d6a660b1eba50ade32640d412bba41cd7b
J Semidey <mtsemidey@gmail.com>
2022-05-03 17:53:36 -0400

    post method created for new candidate

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // import mysql2 package
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -23,10 +24,10 @@ const db = mysql.createConnection(
 );
 
 // return all data from candidates table
-// query() method runs the SQL query and executes the callback with all the resulting rows that match
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT * FROM candidates`;
-
+    
+    // query() method runs the SQL query and executes the callback with all the resulting rows that match
     db.query(sql, (err, rows) => {
         if (err) {
             // 500 = server error, return statement to exit call
@@ -42,7 +43,9 @@ app.get('/api/candidates', (req, res) => {
 
 // return single record from candidates table
 app.get('/api/candidate/:id', (req, res) => {
+    // ? denotes a placeholder, making this a prepared statement
     const sql = `SELECT * FROM candidates WHERE id = ?`;
+    // (param) argument can be an array that holds multiple values for the multiple placeholders
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -80,28 +83,29 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
 });
 
-// // DELETE a candidate from table
-// // ? denotes a placeholder, making this a prepared statement
-// // additional (param) argument following statement provides values to use in place of the placeholders
-// // (param) argument can be an array that holds multiple values for the multiple placeholders
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
 // CREATE a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-// VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+      VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: `Success`,
+            data: body
+        });
+    });
+});
 
 // (catchall route) handle unsupported user requests (404 Not Found)
 app.use((req, res) => {
