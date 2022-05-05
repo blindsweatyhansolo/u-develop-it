ed27e983e510e73f96f8898f2a14526d58944ae9
J Semidey <mtsemidey@gmail.com>
2022-05-05 15:29:34 -0400

    routes for votes

---
+++ routes/apiRoutes/voteRoutes.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const db = require('../../db/connection');
+const router = express.Router();
+const inputCheck = require('../../utils/inputCheck');
+
+// GET return votes, grouped by candidate_id in desc order
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, 
+                COUNT(candidate_id) 
+                AS count FROM votes
+                LEFT JOIN candidates ON votes.candidate_id = candidates.id
+                LEFT JOIN parties ON candidates.party_id = parties.id
+                GROUP BY candidate_id 
+                ORDER BY count DESC`;
+    
+    db.query(sql, (err, rows) =>{
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: `Success`,
+            data: rows
+        });
+    });
+});
+
+// POST cast ballot with voter_id and candidate_id
+router.post('/vote', ({ body }, res) => {
+    // data validation
+    const errors = inputCheck(body, 'voter_id', 'candidate_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?, ?)`;
+    const params = [body.voter_id, body.candidate_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: `Success`,
+            data: body,
+            changes: result.affectedRows
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
